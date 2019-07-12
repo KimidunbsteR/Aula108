8cb5b54d327b1da6204f37c226ae7b8d58c8d3fe
sara pereira <goncalvesarag@gmail.com>
2019-07-12 12:04:52 -0300

    intensivão do JS

---
+++ script.js
@@ -0,0 +1,109 @@
+// function procurarPalavra(palavras, palavraBuscada)
+
+/* TENTATIVA MINHA
+let palavras = ['carro', 'batata', 'Feijão', ''];
+let palavraBuscada = ['batata'];
+
+function verificarIgual(){
+    for(let palavraBuscada === palavras){
+        if (true){
+            return (false)
+        }
+    }
+}*/
+
+/*
+let palavras = ['carro', 'batata', 'feijão'];
+let palavraBuscada = ['batata'];
+
+fuction contem (palavras, palavraBuscada){
+    return palavras.includes(palavraBuscada)
+}
+
+        // console.log('Achamos a palavra solicitada')
+        // {return console.log('Não achei porra nenhuma')
+
+/*function escolherMaior(numero1, numero2){
+    //let maior = maior = numero1 > numero2 ? numero1 : numero2;
+
+    if(numero1 > numero2){
+        return numero1;
+    }
+        return numero2;
+}*/
+
+/*let numero = 0;
+
+function teste(){
+    let numero = 10;
+    console.log('Foi criada uma variável chamada "número" com o valor ' + numero);
+}*/
+
+// let nomes = ['Rafael', 'Leonardo', 'Caroline', 'Ana', 'Gabriela', 'Você'];
+
+/*let DizerOi = () => {
+    alert('Oi');
+    alert('Seja bem vindo');
+}
+
+DizerOi()
+DizerOi()
+DizerOi()*/
+
+/*for(let i = 100; i > 0; i++){
+    console.log(i);
+}*/
+
+/*for(let i = 0; i < 100; i++){
+    console.log(i);
+}
+
+let escolha = '1';
+
+while(escolha !== '0'){
+    escolha = prompt('Digite 0 para sair, ou qualquer outro número, para contiunar')
+}
+
+let nome = 'Trevas Gvng';
+let contador = 1;
+
+do{
+    console.log(contador);
+    contador++;
+}while(contador <= 1000)
+
+let estaChovendo = false;
+let numeros = [
+    [1, 2],
+    [3, 4]
+];
+
+let vetorInterno = numeros[1];
+
+console.log(numeros[1][1]);
+console.log(vetorInterno[1]);
+
+console.log(numeros[3]);
+
+int numero = 1.1;
+typeof 1 -> Number
+typeof 'Trevas Gvng' -> String
+'1' + '1' = '11'
+1 + 1 = 2
+
+let n = 1;
+n == '1' -> true;
+n === '1' -> false;
+n !== '1' -> true;
+
+let idade = prompt('Digite a porra da sua idade!!!')
+
+if(idade >= 30){
+    alert('Seu idoso, sai do trap carai!');
+}
+else if(idade >= 25){
+    alert('Ta na idade boa')
+}
+else {
+    alert('Sai tiw, tu tem cheiro de minecraft')
+}*/
